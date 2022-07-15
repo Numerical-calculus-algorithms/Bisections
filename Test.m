98713ce5250eaf373d654b5a381ea58e5fc3a43e
Vito Proscia <vitoproscia.1704@gmail.com>
2022-07-15 12:37:56 +0200

    Aggiunto calcolo del residuo

--- Test.m
+++ Test.m
@@ -4,7 +4,7 @@ a = 1;
 b = 2;
 tol = 1e-10;
 maxIter = 100;
-fprintf("<strong>Passo\t    Valore x \tStima errore</strong>\n")
+fprintf("<strong>Passo\t   Valore x \tStima errore\t   Residuo</strong>\n")
 Bisection(f,a,b,tol,maxIter);
 
 %% Secondo test metodo delle successive bisezioni
@@ -13,7 +13,7 @@ a = 1;
 b = 2;
 tol = 1e-10;
 maxIter = 100;
-fprintf("<strong>Passo\t    Valore x \tStima errore</strong>\n")
+fprintf("<strong>Passo\t   Valore x \tStima errore\t   Residuo</strong>\n")
 Bisection(f,a,b,tol,maxIter);
 
 %% Terzo test metodo delle successive bisezioni
@@ -22,5 +22,5 @@ a = -1;
 b = 1;
 tol = 1e-10;
 maxIter = 100;
-fprintf("<strong>Passo\t    Valore x \tStima errore</strong>\n")
+fprintf("<strong>Passo\t   Valore x \tStima errore\t   Residuo</strong>\n")
 Bisection(f,a,b,tol,maxIter);
